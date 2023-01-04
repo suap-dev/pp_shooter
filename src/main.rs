16a8a291acaeae9ab61f6387633408b5a8a42477
sleepymarcy <martyna.sowinska@hotmail.com>
2023-01-04 13:53:57 +0100

    remove redundancies

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,7 @@ fn main() {
     let mut engine = ConsoleEngine::init_fill(2).unwrap();
 
     loop {
-        // ConsoleEngine::wait_frame(&mut engine);
         engine.wait_frame();
-        // if ConsoleEngine::is_key_pressed(&engine, KeyCode::Esc){
         if engine.is_key_pressed(KeyCode::Esc) {
             break;
         }
