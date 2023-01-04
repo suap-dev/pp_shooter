8d42a84f5daeeb7631e397f4a8022074a19b535a
sleepymarcy <martyna.sowinska@hotmail.com>
2023-01-04 14:05:38 +0100

    simple game loop

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,25 @@ use console_engine::ConsoleEngine;
 use console_engine::KeyCode;
 
 fn main() {
-    let mut engine = ConsoleEngine::init_fill(2).unwrap();
+    let mut engine = ConsoleEngine::init_fill(15).unwrap();
+    engine.print(0,0, "Hello, world!");
+    engine.draw();
 
-    loop {
+    loop {   
+        // to czyści ekran ze wszystkiego
+        engine.clear_screen();
+
+        // tutaj będziemy tworzyć rysunek
+        engine.print(0,0, "Siusiak");
+
+        // tutaj rysujemy stworzony rysunek
+        engine.draw();
+
+        // wstrzymujemy bieg programu do następnej klatki, by...
         engine.wait_frame();
+        // ...obsłużyć logikę, klawisze, matematykę, itp:
         if engine.is_key_pressed(KeyCode::Esc) {
             break;
         }
     }
-
-        
-    println!("Hello, world!");
-
-
 }
