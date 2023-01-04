349fd5cfcf29226ecebb75028ce6cfad9e142828
sleepymarcy <martyna.sowinska@hotmail.com>
2023-01-04 14:20:48 +0100

    pp game

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,43 @@ use console_engine::KeyCode;
 
 fn main() {
     let mut engine = ConsoleEngine::init_fill(15).unwrap();
-    engine.print(0,0, "Hello, world!");
-    engine.draw();
+    
+    // playable persona
+    let pp = "c==3";
+
+    // pp coords
+    let mut pp_x = 0;
+    let mut pp_y = 0;
 
     loop {   
-        // to czyści ekran ze wszystkiego
+        // this clears everything
         engine.clear_screen();
 
-        // tutaj będziemy tworzyć rysunek
-        engine.print(0,0, "Siusiak");
+        // we are going to create a picture
+        engine.print(
+            pp_x,
+            pp_y,
+            pp
+        );
 
-        // tutaj rysujemy stworzony rysunek
+        // here we are drawing created picture
         engine.draw();
 
-        // wstrzymujemy bieg programu do następnej klatki, by...
+        // pausing program till the next frame, so we can...
         engine.wait_frame();
-        // ...obsłużyć logikę, klawisze, matematykę, itp:
+        // ...handle the logic, keys, maths, etc.
+        if engine.is_key_pressed(KeyCode::Right) {
+            pp_x = pp_x + 1;
+        }
+        if engine.is_key_pressed(KeyCode::Left) {
+            pp_x = pp_x - 1;
+        }
+        if engine.is_key_pressed(KeyCode::Up) {
+            pp_y = pp_y - 1;
+        }
+        if engine.is_key_pressed(KeyCode::Down) {
+            pp_y = pp_y + 1;
+        }
         if engine.is_key_pressed(KeyCode::Esc) {
             break;
         }
