36d448a7b0bcf9664697cbf57c1920e684335533
sleepymarcy <martyna.sowinska@hotmail.com>
2023-01-05 15:39:56 +0100

    Right and left

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use console_engine::ConsoleEngine;
 use console_engine::KeyCode;
 
+const LEFT: usize = 0;
+const RIGHT: usize = 1;
+
+const PP: [&str; 2] = ["c==3", "Ɛ==ↄ"];
+
 fn main() {
     let mut engine = ConsoleEngine::init_fill(15).unwrap();
-    
-    // playable persona
-    let pp = "c==3";
-
+   
     // pp coords
     let mut pp_x = 0;
     let mut pp_y = 0;
+
+    // pp facing
+    let mut facing = LEFT;
 
     loop {   
         // this clears everything
@@ -19,7 +24,7 @@ fn main() {
         engine.print(
             pp_x,
             pp_y,
-            pp
+            PP[facing]
         );
 
         // here we are drawing created picture
@@ -27,17 +32,20 @@ fn main() {
 
         // pausing program till the next frame, so we can...
         engine.wait_frame();
+
         // ...handle the logic, keys, maths, etc.
-        if engine.is_key_pressed(KeyCode::Right) {
-            pp_x = pp_x + 1;
+        if engine.is_key_held(KeyCode::Right) {
+            facing = RIGHT;
+            pp_x += 1;
         }
-        if engine.is_key_pressed(KeyCode::Left) {
+        if engine.is_key_held(KeyCode::Left) {
+            facing = LEFT;
             pp_x = pp_x - 1;
         }
-        if engine.is_key_pressed(KeyCode::Up) {
+        if engine.is_key_held(KeyCode::Up) {
             pp_y = pp_y - 1;
         }
-        if engine.is_key_pressed(KeyCode::Down) {
+        if engine.is_key_held(KeyCode::Down) {
             pp_y = pp_y + 1;
         }
         if engine.is_key_pressed(KeyCode::Esc) {
