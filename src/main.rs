23c9b565d657842bf5fe4808b6f9d7dfe7db8349
sleepymarcy <martyna.sowinska@hotmail.com>
2023-01-05 16:24:00 +0100

    beautified
    with struct and enum

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,22 @@
-use console_engine::ConsoleEngine;
-use console_engine::KeyCode;
+use console_engine::{ ConsoleEngine, KeyCode };
 
-const LEFT: usize = 0;
-const RIGHT: usize = 1;
-
-const PP: [&str; 2] = ["c==3", "Ɛ==ↄ"];
 
 fn main() {
-    let mut engine = ConsoleEngine::init_fill(15).unwrap();
-   
-    // pp coords
-    let mut pp_x = 0;
-    let mut pp_y = 0;
+    let mut engine = ConsoleEngine::init_fill(30).unwrap();
 
-    // pp facing
-    let mut facing = LEFT;
+    let mut pp = Player{
+        model: [String::from("c==3"), String::from("Ɛ==ↄ")],
+        facing: Facing::Left,
+        position: Coords{x: 1, y: 2}
+    };       
 
     loop {   
         // this clears everything
         engine.clear_screen();
 
         // we are going to create a picture
-        engine.print(
-            pp_x,
-            pp_y,
-            PP[facing]
-        );
-
+        pp.draw(&mut engine);
+       
         // here we are drawing created picture
         engine.draw();
 
@@ -34,22 +24,54 @@ fn main() {
         engine.wait_frame();
 
         // ...handle the logic, keys, maths, etc.
-        if engine.is_key_held(KeyCode::Right) {
-            facing = RIGHT;
-            pp_x += 1;
-        }
-        if engine.is_key_held(KeyCode::Left) {
-            facing = LEFT;
-            pp_x = pp_x - 1;
-        }
-        if engine.is_key_held(KeyCode::Up) {
-            pp_y = pp_y - 1;
-        }
-        if engine.is_key_held(KeyCode::Down) {
-            pp_y = pp_y + 1;
-        }
-        if engine.is_key_pressed(KeyCode::Esc) {
-            break;
-        }
+        if engine.is_key_held(KeyCode::Right) { pp.go_right(); }
+        if engine.is_key_held(KeyCode::Left) { pp.go_left(); }
+        if engine.is_key_held(KeyCode::Up) { pp.go_up(); }
+        if engine.is_key_held(KeyCode::Down) { pp.go_down(); }
+        if engine.is_key_pressed(KeyCode::Esc) { break; }
     }
 }
+
+struct Player {
+    model: [String; 2],
+    facing: Facing,
+    position: Coords,
+}
+impl Player {
+    fn go_right(&mut self){
+        self.position.x += 1;
+        self.facing = Facing::Right;
+    }
+
+    fn go_left(&mut self){
+        self.position.x -= 1;
+        self.facing = Facing::Left;
+    }
+
+    fn go_up(&mut self){
+        self.position.y -= 1;
+    }
+
+    fn go_down(&mut self){
+        self.position.y += 1;
+    }
+
+    fn draw(&self, engine: &mut ConsoleEngine){
+        engine.print(
+            self.position.x,
+            self.position.y,
+            self.model[self.facing as usize].as_str() ,
+        );
+    }
+}
+
+struct Coords {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Clone, Copy)]
+enum Facing {
+    Left = 0,
+    Right = 1,
+} 
