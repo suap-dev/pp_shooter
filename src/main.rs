a056e1afb1b4b37399beeba8d78551b62cfe7ec3
sleepymarcy <martyna.sowinska@hotmail.com>
2023-01-06 18:41:39 +0100

    projectile spawning

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,48 @@
 use console_engine::{ ConsoleEngine, KeyCode };
 
+// TODO: Make the shooting mechanism work correctly... Now it just spawns a projectile attached to the tip.
 
 fn main() {
-    let mut engine = ConsoleEngine::init_fill(30).unwrap();
+    let mut screen = ConsoleEngine::init_fill(30).unwrap();
 
-    let mut pp = Player{
+    let mut p1 = Player{
         model: [String::from("c==3"), String::from("Ɛ==ↄ")],
         facing: Facing::Left,
-        position: Coords{x: 1, y: 2}
-    };       
+        position: Coords{x: 1, y: 2},
+        projectile: String::from("-"),  // length of this string should be taken into consideration
+                                        // when shooting
+        is_shooting: false,
+    };
 
     loop {   
         // this clears everything
-        engine.clear_screen();
+        screen.clear_screen();
 
         // we are going to create a picture
-        pp.draw(&mut engine);
+        p1.add_to_picture(&mut screen);
        
         // here we are drawing created picture
-        engine.draw();
+        screen.draw();
 
         // pausing program till the next frame, so we can...
-        engine.wait_frame();
+        screen.wait_frame();
 
         // ...handle the logic, keys, maths, etc.
-        if engine.is_key_held(KeyCode::Right) { pp.go_right(); }
-        if engine.is_key_held(KeyCode::Left) { pp.go_left(); }
-        if engine.is_key_held(KeyCode::Up) { pp.go_up(); }
-        if engine.is_key_held(KeyCode::Down) { pp.go_down(); }
-        if engine.is_key_pressed(KeyCode::Esc) { break; }
+        if screen.is_key_held(KeyCode::Right) { p1.go_right(); }
+        if screen.is_key_held(KeyCode::Left) { p1.go_left(); }
+        if screen.is_key_held(KeyCode::Up) { p1.go_up(); }
+        if screen.is_key_held(KeyCode::Down) { p1.go_down(); }
+        if screen.is_key_held(KeyCode::Enter) { p1.start_shooting(); }
+        if screen.is_key_pressed(KeyCode::Esc) { break; }
     }
 }
 
 struct Player {
     model: [String; 2],
+    projectile: String,
     facing: Facing,
     position: Coords,
+    is_shooting: bool,
 }
 impl Player {
     fn go_right(&mut self){
@@ -55,12 +62,46 @@ impl Player {
     fn go_down(&mut self){
         self.position.y += 1;
     }
+    
+    fn start_shooting(&mut self){
+        self.is_shooting = true;
+    }
 
-    fn draw(&self, engine: &mut ConsoleEngine){
-        engine.print(
+    fn stop_shooting(&mut self){
+        self.is_shooting = false;
+    }
+    
+    fn get_barrel_exit_coords(&self) -> Coords{
+        // this is very dirty
+        // TODO: make it better...
+        let mut len = 0;
+        for _ in self.model[1].chars() {
+            len += 1;
+        }
+
+        // this is very... situational...
+        let x: i32 = match self.facing {
+            Facing::Right => self.position.x + len as i32,
+            Facing::Left => self.position.x - 1,
+        };
+
+        let y: i32 = self.position.y;
+
+        Coords{x, y}
+    }
+
+    fn add_to_picture(&self, screen: &mut ConsoleEngine){
+        screen.print(
             self.position.x,
             self.position.y,
-            self.model[self.facing as usize].as_str() ,
+            self.model[self.facing as usize].as_str(),
+        );
+
+        let barrel_exit = self.get_barrel_exit_coords();
+        screen.print(
+            barrel_exit.x,
+            barrel_exit.y,
+            self.projectile.as_str(),
         );
     }
 }
@@ -72,6 +113,6 @@ struct Coords {
 
 #[derive(Clone, Copy)]
 enum Facing {
-    Left = 0,
-    Right = 1,
-} 
+    Left,
+    Right,
+}
