8cf67edba77a91fed86ec622548a23055bbe0f75
suap-dev <suap.dev@gmail.com>
2023-01-08 16:10:53 +0100

    making Vec2 more functional

--- src/data_structs.rs
+++ src/data_structs.rs
@@ -1,9 +1,23 @@
-use std::ops::{Add, Sub};
+use std::ops::{Add, Div, Mul, Sub};
 
 #[derive(Clone, Copy)]
 pub struct Vec2 {
     pub x: f32,
     pub y: f32,
+}
+impl Vec2 {
+    pub fn new(x: f32, y: f32) -> Self {
+        Self { x, y }
+    }
+    pub fn inew(x: i32, y: i32) -> Self {
+        Self::new(x as f32, y as f32)
+    }
+    pub fn zero() -> Self {
+        Self { x: 0.0, y: 0.0 }
+    }
+    pub fn one() -> Self {
+        Self { x: 1.0, y: 1.0 }
+    }
 }
 impl Add for Vec2 {
     type Output = Self;
@@ -21,11 +35,70 @@ impl Sub for Vec2 {
     fn sub(self, rhs: Self) -> Self::Output {
         Self::Output {
             x: self.x - rhs.x,
-            y: self.y - rhs.y
+            y: self.y - rhs.y,
         }
     }
 }
+impl Mul<f32> for Vec2 {
+    type Output = Self;
 
+    fn mul(self, rhs: f32) -> Self::Output {
+        Self::Output {
+            x: self.x * rhs,
+            y: self.y * rhs,
+        }
+    }
+}
+impl Mul<Vec2> for f32 {
+    type Output = Vec2;
+
+    fn mul(self, rhs: Vec2) -> Vec2 {
+        Vec2 {
+            x: self * rhs.x,
+            y: self * rhs.y,
+        }
+    }
+}
+impl Mul<i32> for Vec2 {
+    type Output = Self;
+
+    fn mul(self, rhs: i32) -> Self::Output {
+        Self::Output {
+            x: self.x * rhs as f32,
+            y: self.y * rhs as f32,
+        }
+    }
+}
+impl Mul<Vec2> for i32 {
+    type Output = Vec2;
+
+    fn mul(self, rhs: Vec2) -> Vec2 {
+        Vec2 {
+            x: self as f32 * rhs.x,
+            y: self as f32 * rhs.y,
+        }
+    }
+}
+impl Div<f32> for Vec2 {
+    type Output = Self;
+
+    fn div(self, rhs: f32) -> Self::Output {
+        Self::Output {
+            x: self.x / rhs,
+            y: self.y / rhs,
+        }
+    }
+}
+impl Div<i32> for Vec2 {
+    type Output = Self;
+
+    fn div(self, rhs: i32) -> Self::Output {
+        Self::Output {
+            x: self.x / rhs as f32,
+            y: self.y / rhs as f32,
+        }
+    }
+}
 
 #[derive(Clone, Copy)]
 pub enum Facing {
@@ -34,5 +107,8 @@ pub enum Facing {
 }
 
 pub enum Direction {
-    Left, Right, Up, Down,
+    Left,
+    Right,
+    Up,
+    Down,
 }
--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,9 @@ fn main() {
     let mut screen = ConsoleEngine::init_fill(60).unwrap();
 
     let mut bullet: Projectile = Projectile {
-        position: Vec2 { x: -1., y: -1. },
+        position: Vec2::inew(-1,-1),
         model: String::from("--"), // length of this string should be taken into consideration
-        velocity: Vec2 { x: 0., y: 0. },
+        velocity: Vec2::zero(),
         base_speed: 2.0,
     };
 
@@ -22,8 +22,8 @@ fn main() {
         model: [String::from("c==3"), String::from("Ɛ==ↄ")],
         facing: Facing::Right,
         direction: Direction::Right,
-        position: Vec2 { x: 8., y: 4. },
-        velocity: Vec2 { x: 0., y: 0. },
+        position: Vec2::inew(8, 4),
+        velocity: Vec2::zero(),
         base_speed: 0.3,
         should_shoot: false,
     };
