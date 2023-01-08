bc3c44ab55dfb813c58bdc1873ea00b2e777a762
suap-dev <suap.dev@gmail.com>
2023-01-08 15:37:08 +0100

    Coords -> Vec2, add Direction

--- src/data_structs.rs
+++ src/data_structs.rs
@@ -1,10 +1,38 @@
-pub struct Coords {
-    pub x: i32,
-    pub y: i32,
+use std::ops::{Add, Sub};
+
+#[derive(Clone, Copy)]
+pub struct Vec2 {
+    pub x: f32,
+    pub y: f32,
 }
+impl Add for Vec2 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::Output {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+impl Sub for Vec2 {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self::Output {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y
+        }
+    }
+}
+
 
 #[derive(Clone, Copy)]
 pub enum Facing {
     Left,
     Right,
+}
+
+pub enum Direction {
+    Left, Right, Up, Down,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod data_structs;
 mod entities;
 
-use data_structs::{Coords, Facing};
+use data_structs::{Direction, Facing, Vec2};
 use entities::{Player, Projectile};
 
 use console_engine::{ConsoleEngine, KeyCode};
@@ -9,18 +9,21 @@ use console_engine::{ConsoleEngine, KeyCode};
 // TODO: Make the shooting mechanism work correctly... Make the projectile move.
 
 fn main() {
-    let mut screen = ConsoleEngine::init_fill(30).unwrap();
+    let mut screen = ConsoleEngine::init_fill(60).unwrap();
 
     let mut bullet: Projectile = Projectile {
-        position: Coords { x: -1, y: -1 },
-        model: String::from("-"), // length of this string should be taken into consideration
-        velocity: 1,
+        position: Vec2 { x: -1., y: -1. },
+        model: String::from("--"), // length of this string should be taken into consideration
+        velocity: Vec2 { x: 0., y: 0. },
+        speed: 1.0,
     };
 
     let mut p1 = Player {
         model: [String::from("c==3"), String::from("Ɛ==ↄ")],
-        facing: Facing::Left,
-        position: Coords { x: 8, y: 4 },
+        facing: Facing::Right,
+        direction: Direction::Right,
+        position: Vec2 { x: 8., y: 4. },
+        velocity: Vec2 { x: 0., y: 0. },
         should_shoot: false,
     };
 
@@ -38,19 +41,36 @@ fn main() {
         // pausing program till the next frame, so we can...
         screen.wait_frame();
 
+        if screen.is_key_pressed(KeyCode::Up) {
+            p1.go(Direction::Up);
+        }
+        if screen.is_key_pressed(KeyCode::Down) {
+            p1.go(Direction::Down);
+        }
+        if screen.is_key_pressed(KeyCode::Right) {
+            p1.go(Direction::Right);
+        }
+        if screen.is_key_pressed(KeyCode::Left) {
+            p1.go(Direction::Left);
+        }
+        if screen.is_key_pressed(KeyCode::Char(' ')) {
+            p1.stop();
+        }
+
         // ...handle the logic, keys, maths, etc.
-        if screen.is_key_held(KeyCode::Right) {
-            p1.go_right();
-        }
-        if screen.is_key_held(KeyCode::Left) {
-            p1.go_left();
-        }
-        if screen.is_key_held(KeyCode::Up) {
-            p1.go_up();
-        }
-        if screen.is_key_held(KeyCode::Down) {
-            p1.go_down();
-        }
+        // if screen.is_key_held(KeyCode::Right) {
+        //     p1.go_right();
+        // }
+        // if screen.is_key_held(KeyCode::Left) {
+        //     p1.go_left();
+        // }
+        // if screen.is_key_held(KeyCode::Up) {
+        //     p1.go_up();
+        // }
+        // if screen.is_key_held(KeyCode::Down) {
+        //     p1.go_down();
+        // }
+
         if screen.is_key_released(KeyCode::Enter) {
             p1.shoot(&mut bullet);
         }
@@ -59,6 +79,7 @@ fn main() {
         }
 
         // game logic
+        p1.proceed_in_time();
         bullet.proceed_in_time();
     }
 }
